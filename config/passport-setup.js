be7b8a03fb7081298270fe633b79de7897d3f075
Abdou Nuru Niyigena <abdou.niyigena@Abdous-MacBook-Pro.local>
2020-01-20 08:55:32 +0200

    setup the project with passport

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,8 +1,11 @@
 import passport from 'passport';
 import GoogleStrategy from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+
+dotenv();
 
 passport.use(new  GoogleStrategy({
-
+    key: process.env.key
 }), () => {
      
     }
