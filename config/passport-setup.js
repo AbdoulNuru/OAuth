9ee35538c647552509351f70607a380945377754
Abdou Nuru Niyigena <abdou.niyigena@Abdous-MacBook-Pro.local>
2020-01-21 16:26:11 +0200

    !!

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,16 +1,48 @@
 import passport from 'passport';
 import GoogleStrategy from 'passport-google-oauth20';
 import dotenv from 'dotenv';
+import { AuthUsers } from '../db/models';
 
 dotenv.config();
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const whichUser = await AuthUsers.findOne({
+    where: {
+      id,
+    },
+    raw: true,
+  });
+
+  done(null, whichUser);
+});
 
 passport.use(
   new GoogleStrategy({
     callbackURL: '/auth/google/redirect',
     clientID: process.env.clientID,
     clientSecret: process.env.clientSecret,
-  }, (accessToken, refreshToken, profile, done) => {
-    console.log('ralalalala');
-    console.log(profile);
+  }, async (accessToken, refreshToken, profile, done) => {
+    const userExist = await AuthUsers.findOne({
+      where: {
+        googleId: profile.id,
+      },
+      raw: true,
+    });
+
+    if (userExist) {
+      console.log(userExist);
+      done(null, userExist);
+    } else {
+      const newUser = await AuthUsers.create({
+        username: profile.displayName,
+        googleId: profile.id,
+      });
+
+      done(null, newUser);
+    }
   }),
 );
